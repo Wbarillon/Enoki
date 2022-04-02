a381f745facc268ef7f8d36dd5399005361ee5d2
Wbarillon <45207821+Wbarillon@users.noreply.github.com>
2022-04-02 16:31:07 +0200

    [2022-04-02 16:30] setting up first quiz
    - gitignore updated without wrong conception
    - models added in admin
    - models added

--- enoki_app/admin.py
+++ enoki_app/admin.py
@@ -1,14 +1,39 @@
 from django.contrib import admin
 
 from enoki_app.models import (
-    CustomUser
+    AnswerChoice,
+    AnswerUser,
+    CustomUser,
+    Question,
+    Quiz,
+    Score
 )
 
 # Register your models here.
 
 from django.contrib import admin
 
+@admin.register(AnswerChoice)
+class AnswerChoiceAdmin(admin.ModelAdmin):
+    list_display = [element.name for element in AnswerChoice._meta.fields]
+
+@admin.register(AnswerUser)
+class AnswerUserAdmin(admin.ModelAdmin):
+    list_display = [element.name for element in AnswerUser._meta.fields]
+
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     # Fields are sliced in order to not display password field.
     list_display = [element.name for element in CustomUser._meta.fields[:1] + CustomUser._meta.fields[2:]]
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [element.name for element in Question._meta.fields]
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [element.name for element in Quiz._meta.fields]
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = [element.name for element in Score._meta.fields]
--- enoki_app/models.py
+++ enoki_app/models.py
@@ -28,3 +28,72 @@ class CustomUser(PermissionsMixin, AbstractBaseUser):
     class Meta:
         verbose_name = 'Utilisateur (custom)'
         verbose_name_plural = 'Utilisateurs (custom)'
+
+class Quiz(models.Model):
+
+    id = models.AutoField(primary_key = True)
+    subject = models.CharField(verbose_name = 'Sujet', max_length = 50, null = True, blank = True)
+    version = models.PositiveSmallIntegerField(verbose_name = 'Version', null = True, blank = True)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Questionnaire'
+        verbose_name_plural = 'Questionnaires'
+
+class Question(models.Model):
+
+    id = models.AutoField(primary_key = True)
+    id_quiz = models.ForeignKey('Quiz', on_delete = models.CASCADE, verbose_name = 'Questionnaire', null = False, blank = False)
+    question = models.CharField(verbose_name = 'Question', max_length = 150, null = True, blank = True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+class AnswerChoice(models.Model):
+
+    id = models.AutoField(primary_key = True)
+    id_quiz = models.ForeignKey('Quiz', on_delete = models.CASCADE, verbose_name = 'Questionnaire', null = False, blank = False)
+    id_question = models.ForeignKey('Question', on_delete = models.CASCADE, verbose_name = 'Question', null = False, blank = False)
+    answer_choice = models.CharField(verbose_name = 'Choix de réponses', max_length = 150, null = True, blank = True)
+    correct_answer = models.BooleanField(verbose_name = 'Bonne réponse', null = True)
+
+    def __str__(self):
+        return self.answer_choice
+
+    class Meta:
+        verbose_name = 'Choix de réponse'
+        verbose_name_plural = 'Choix de réponses'
+
+class AnswerUser(models.Model):
+
+    id = models.AutoField(primary_key = True)
+    id_user = models.ForeignKey('CustomUser', on_delete = models.PROTECT, verbose_name = 'Utilisateur', null = False, blank = False)
+    id_quiz = models.ForeignKey('Quiz', on_delete = models.CASCADE, verbose_name = 'Questionnaire', null = False, blank = False)
+    id_question = models.ForeignKey('Question', on_delete = models.CASCADE, verbose_name = 'Question', null = False, blank = False)
+    id_answer_choice = models.ForeignKey('AnswerChoice', on_delete = models.CASCADE, verbose_name = 'Choix de réponse', null = False, blank = False)
+
+    def __str__(self):
+        return self.id_user + - + self.id_answer_choice
+
+    class Meta:
+        verbose_name = 'Réponse des utilisateurs'
+        verbose_name_plural = 'Réponses des utilisateurs'
+
+class Score(models.Model):
+
+    id = models.AutoField(primary_key = True)
+    id_user = models.ForeignKey('CustomUser', on_delete = models.PROTECT, verbose_name = 'Utilisateur', null = False, blank = False)
+    score = models.DecimalField(verbose_name = 'Score', max_digits = 3, decimal_places = 2, null = True, blank = True)
+
+    def __str__(self):
+        return self.id_user + - + score
+
+    class Meta:
+        verbose_name = 'Score'
+        verbose_name_plural = 'Scores'
